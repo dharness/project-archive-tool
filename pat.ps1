8057513231e7644d90082bbb8ee6e9c34975c750
Dylan Harness <dharness.engineer@gmail.com>
2022-09-23 15:50:07 -0700

    .

--- pat.ps1
+++ pat.ps1
@@ -1,3 +1,7 @@
+<#
+This script is added to the windows registry to provide a context
+menu for running PAT. See README for details
+#>
 cd $PSScriptRoot
 .venv/Scripts/activate.ps1
 $script = $PSScriptRoot + "\project_archive_tool\pat.py"
